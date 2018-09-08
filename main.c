4009f384aff3c95b9cbb8fbbfd9af33dcf453eb7
Filippo maria COPPOLA <ficoppol@e2r12p2.42.fr>
2018-09-08 19:26:18 +0200

    aaaa

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: ficoppol <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/08 11:23:24 by ficoppol          #+#    #+#             */
-/*   Updated: 2018/09/08 12:13:53 by ficoppol         ###   ########.fr       */
+/*   Updated: 2018/09/08 19:26:02 by ficoppol         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,13 +22,28 @@ int		ft_strcmp(char *s1, char *s2)
 	while (s1[i])
 	{
 		if (s1[i] == s2[i])
-			j++;
-		else if (s2[j] == '*')
 		{
-			while (s2[j] == '*')
-				j++;
-			i = j - 1;
+			i++;
+			j++;
 		}
-		i++;
+		else
+		{
+			if (s2[j] == '*')
+			{
+				j++;
+				while (s1[i] != s2[j])
+					i++;
+			}
+			i++;
+		}
 	}
+	return (s1[i] - s2[j]);
+}
+
+int		main(void)
+{
+	char s1[6] = "Hello";
+	char s2[6] = "H*lo";
+	printf("%d", ft_strcmp(s1, s2));
+	return (0);
 }
