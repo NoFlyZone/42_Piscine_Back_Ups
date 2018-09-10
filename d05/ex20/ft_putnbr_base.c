a871a626cfe1b4069b99aac29b51e774f65c63dd
Filippo Maria Coppola <filippomariacoppola@macbook-air-di-filippo.home>
2018-09-11 00:40:43 +0200

    aaaa

---
+++ d05/ex20/ft_putnbr_base.c
@@ -0,0 +1,1 @@
+// allora coglione, dividi il numero per la lunghezza della base per la ricorsivita, dopodiche prendi il resto e lo stampi facendo riferimento alla stringa della base, e di caratteri non di numeri;
